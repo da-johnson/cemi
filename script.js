561a297985ff4ebb955b2ff7209e8505cad93020
Alex Johnson <91097733+da-johnson@users.noreply.github.com>
2023-03-28 12:07:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-window.onload = () => {
-  const gridItems = document.querySelectorAll('.grid-item');
+const gridItems = document.querySelectorAll('.grid-item');
 
-  gridItems.forEach(item => {
-    item.addEventListener('click', () => {
-      const title = item.dataset.title;
-      const url = item.dataset.url;
-      const metadata = document.querySelector('.metadata');
-      metadata.innerHTML = `
-        <h2>${title}</h2>
-        <ul>
-          <li><a href="${url}" target="_blank">${url}</a></li>
-        </ul>
-      `;
-      metadata.style.display = 'block';
-    });
-  });
-
-  // hide metadata on click outside of the grid or metadata
-  document.addEventListener('click', (event) => {
-    const isGridItem = event.target.classList.contains('grid-item');
-    const isMetadata = event.target.classList.contains('metadata');
+gridItems.forEach((item, index) => {
+  item.setAttribute('data-index', index + 1); /* update data-index */
+  item.addEventListener('click', () => {
+    const title = item.dataset.title;
+    const url = item.dataset.url;
     const metadata = document.querySelector('.metadata');
-    if (!isGridItem && !isMetadata) {
-      metadata.style.display = 'none';
-    }
+    metadata.innerHTML = `
+      <h2>${title}</h2>
+      <ul>
+        <li><a href="${url}" target="_blank">${url}</a></li>
+      </ul>
+    `;
+    metadata.style.display = 'block';
+    metadata.style.top = `${event.pageY + 10}px`; /* add 10px to position metadata box below mouse */
+    metadata.style.left = `${event.pageX + 10}px`; /* add 10px to position metadata box to the right of mouse */
   });
-}
+});
+
+/* ... */
