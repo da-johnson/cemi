974982ee9a25dfa9a7e974dd4fddff4cf40fd00c
Alex Johnson <91097733+da-johnson@users.noreply.github.com>
2023-03-28 11:47:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-const gridItems = document.querySelectorAll('.grid-item');
+window.onload = () => {
+  const gridItems = document.querySelectorAll('.grid-item');
 
-gridItems.forEach(item => {
-  item.addEventListener('click', () => {
-    const title = item.dataset.title;
-    const url = item.dataset.url;
-    const metadata = document.querySelector('.metadata');
-    metadata.innerHTML = `
-      <h2>${title}</h2>
-      <ul>
-        <li><a href="${url}" target="_blank">${url}</a></li>
-      </ul>
-    `;
-    metadata.style.display = 'block';
+  gridItems.forEach(item => {
+    item.addEventListener('click', () => {
+      const title = item.dataset.title;
+      const url = item.dataset.url;
+      const metadata = document.querySelector('.metadata');
+      metadata.innerHTML = `
+        <h2>${title}</h2>
+        <ul>
+          <li><a href="${url}" target="_blank">${url}</a></li>
+        </ul>
+      `;
+      metadata.style.display = 'block';
+    });
   });
-});
 
-// hide metadata on click outside of the grid or metadata
-document.addEventListener('click', (event) => {
-  const isGridItem = event.target.classList.contains('grid-item');
-  const isMetadata = event.target.classList.contains('metadata');
-  const metadata = document.querySelector('.metadata');
-  if (!isGridItem && !isMetadata) {
-    metadata.style.display = 'none';
-  }
-});
+  // hide metadata on click outside of the grid or metadata
+  document.addEventListener('click', (event) => {
+    const isGridItem = event.target.classList.contains('grid-item');
+    const isMetadata = event.target.classList.contains('metadata');
+    const metadata = document.querySelector('.metadata');
+    if (!isGridItem && !isMetadata) {
+      metadata.style.display = 'none';
+    }
+  });
+}
