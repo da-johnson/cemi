fb58d2b6e8ea66675aa3b31ac87705abf1d6ed9e
Alex Johnson <91097733+da-johnson@users.noreply.github.com>
2023-03-28 17:17:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const gridItems = document.querySelectorAll('.grid-item');
-const grid = document.querySelector('.grid');
 const metadata = document.querySelector('.metadata');
 
 gridItems.forEach((item, index) => {
   item.setAttribute('data-index', index + 1);
-  item.addEventListener('click', () => {
+  item.addEventListener('click', (event) => {
     const title = item.dataset.title;
     const url = item.dataset.url;
     metadata.innerHTML = `
@@ -14,9 +13,18 @@ gridItems.forEach((item, index) => {
       </ul>
     `;
     metadata.style.display = 'block';
-    const rect = grid.getBoundingClientRect();
-    const bottom = rect.top + rect.height;
-    metadata.style.top = `${bottom + 10}px`;
-    metadata.style.left = `${item.offsetLeft}px`;
+    metadata.style.top = `${event.pageY + 10}px`;
+    metadata.style.left = `${event.pageX + 10}px`;
+    
+    // Check if the metadata block extends beyond the visible screen area
+    const metadataBottom = metadata.offsetTop + metadata.offsetHeight;
+    const visibleScreenBottom = window.pageYOffset + window.innerHeight;
+    const distanceToBottom = metadataBottom - visibleScreenBottom;
+    
+    if (distanceToBottom > 0) {
+      // Adjust the position of the metadata block to fit within the visible screen area
+      metadata.style.top = `${event.pageY - metadata.offsetHeight - 20}px`;
+    }
   });
 });
+
